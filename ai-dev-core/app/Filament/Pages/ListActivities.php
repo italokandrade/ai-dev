c37124ead16f5a0f7f222534fae195a287307a95
italokandrade <italokristiano@gmail.com>
2026-04-21 14:50:26 -0300

    feat: register and customize official Activity Log page in Administração group

---
+++ ai-dev-core/app/Filament/Pages/ListActivities.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use pxlrbt\FilamentActivityLog\Pages\ListActivities as BaseListActivities;
+
+class ListActivities extends BaseListActivities
+{
+    protected static ?string $navigationLabel = 'Logs de Atividades';
+
+    protected static ?string $title = 'Logs de Atividades';
+
+    protected static string|\UnitEnum|null $navigationGroup = 'Administração';
+
+    protected static ?int $navigationSort = 100; // Abaixo de Usuários (80)
+}
